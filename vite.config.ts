9ce84e022200b0053c1deac232af959f18c508db
CJ Harries <cj@wizardsoftheweb.pro>
2023-12-16 18:55:08 -0600

    Set up library build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [dts(), react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/index.ts"),
+      name: "task-timer-library",
+      fileName: "task-timer-library",
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+});
